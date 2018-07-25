1facb3d01cdd6e8fb145f85ccfcf49dc8c024d0b
Peter <yzhou09@mail.uoguelph.ca>
2018-07-25 02:42:11 -0400

    learing redux with react now

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Component } from 'react';
+import BookList from '../containers/book-list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <BookList/>
+    );
+  }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+  return [
+      {title: 'peter'},
+      {title: 'randy'},
+      {title: 'jacky'},
+      {title: 'patrick'},
+      {title: 'hoaran'}
+    ]
+}
